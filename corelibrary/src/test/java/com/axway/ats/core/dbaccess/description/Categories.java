d86689e563105a6c84a02c493f9fa390146ef115
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-09-10 23:53:48 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4-turbo

---
+++ corelibrary/src/test/java/com/axway/ats/core/dbaccess/description/Categories.java
@@ -0,0 +1,101 @@
+package com.axway.ats.core.dbaccess.description;
+
+public interface Categories {
+
+	interface valid {
+
+	}
+
+	interface invalid {
+
+	}
+
+	interface boundary {
+
+	}
+
+	interface integration {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface getType {
+
+	}
+
+	interface getType {
+
+	}
+
+	interface getFieldName {
+
+	}
+
+	interface getIsNull {
+
+	}
+
+	interface getKey {
+
+	}
+
+	interface getDefaultValue {
+
+	}
+
+	interface getExtra {
+
+	}
+
+	interface getDifferences {
+
+	}
+
+	interface add {
+
+	}
+
+	interface add {
+
+	}
+
+	interface add {
+
+	}
+
+	interface add {
+
+	}
+
+	interface add {
+
+	}
+
+	interface add {
+
+	}
+
+	interface getDataAsString {
+
+	}
+
+	interface toString {
+
+	}
+
+	interface getName {
+
+	}
+
+	interface get {
+
+	}
+
+	interface getColumns {
+
+	}
+
+}
